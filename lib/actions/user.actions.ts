1cefb20e25baec42bb6e70e26c2f5c419693a42f
arstan001 <arstanashyrbekov@gmail.com>
2023-10-13 21:37:49 +0800

    activity page and search

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.models";
 import { connectToDB } from "../mongoose"
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 interface Params {
     userId: string,
     username: string,
@@ -80,3 +81,84 @@ export async function fetchUserPosts(userId: string) {
         throw new Error(`Failed to fetch user:${error?.message}`)
     }
 }
+export async function fetchUsers({
+    userId,
+    searchString = "",
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = "desc",
+}: {
+    userId: string;
+    searchString?: string;
+    pageNumber?: number;
+    pageSize?: number;
+    sortBy?: SortOrder;
+}) {
+    try {
+        connectToDB();
+
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        // Create a case-insensitive regular expression for the provided search string.
+        const regex = new RegExp(searchString, "i");
+
+        // Create an initial query object to filter users.
+        const query: FilterQuery<typeof User> = {
+            id: { $ne: userId }, // Exclude the current user from the results.
+        };
+
+        // If the search string is not empty, add the $or operator to match either username or name fields.
+        if (searchString.trim() !== "") {
+            query.$or = [
+                { username: { $regex: regex } },
+                { name: { $regex: regex } },
+            ];
+        }
+        const sortOptions = { createdAt: sortBy };
+
+        const usersQuery = User.find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize);
+
+        // Count the total number of users that match the search criteria (without pagination).
+        const totalUsersCount = await User.countDocuments(query);
+
+        const users = await usersQuery.exec();
+
+        // Check if there are more users beyond the current page.
+        const isNext = totalUsersCount > skipAmount + users.length;
+
+        return { users, isNext };
+    } catch (error: any) {
+        throw new Error(`Failed to fetch users:${error?.message}`)
+    }
+}
+export async function getActivity(userId: string) {
+    try {
+      connectToDB();
+  
+      // Find all threads created by the user
+      const userThreads = await Thread.find({ author: userId });
+  
+      // Collect all the child thread ids (replies) from the 'children' field of each user thread
+      const childThreadIds = userThreads.reduce((acc, userThread) => {
+        return acc.concat(userThread.children);
+      }, []);
+  
+      // Find and return the child threads (replies) excluding the ones created by the same user
+      const replies = await Thread.find({
+        _id: { $in: childThreadIds },
+        author: { $ne: userId }, // Exclude threads authored by the same user
+      }).populate({
+        path: "author",
+        model: User,
+        select: "name image _id",
+      });
+  
+      return replies;
+    } catch (error) {
+      console.error("Error fetching replies: ", error);
+      throw error;
+    }
+  }
