c0af89766c8174da09cc99a63cee585b4e88519e
arstan001 <arstanashyrbekov@gmail.com>
2023-10-13 20:56:33 +0800

    profile page added

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,50 +3,80 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.models";
 import { connectToDB } from "../mongoose"
+import Thread from "../models/thread.model";
 interface Params {
-    userId:string,
-    username:string,
-    name:string,
-    bio:string,
-    image:string,
-    path:string
+    userId: string,
+    username: string,
+    name: string,
+    bio: string,
+    image: string,
+    path: string
 }
-export async function updateUser({userId, username, name, bio, image, path}:Params):Promise<void>{
+export async function updateUser({ userId, username, name, bio, image, path }: Params): Promise<void> {
     connectToDB();
 
     try {
         await User.findOneAndUpdate(
-            {id:userId}, 
+            { id: userId },
             {
-                username:username.toLowerCase(),
+                username: username.toLowerCase(),
                 name,
                 bio,
                 image,
-                onboarded:true
+                onboarded: true
             },
-            {upsert:true}
+            { upsert: true }
         )
-    
-        if(path==='/profile/edit'){
+
+        if (path === '/profile/edit') {
             revalidatePath(path);
         }
     } catch (error: any) {
         // throw new Error("Failed to create update user", error.message);
         console.log('error', error?.message)
-        
+
     }
 }
-export async function fetchUser(userId:string) {
+export async function fetchUser(userId: string) {
     try {
         connectToDB();
 
         return await User
-            .findOne({id:userId})
-            // .populate({
-            //     path:'Communities',
-            //     model:Community
-            // })
-    } catch (error:any) {
+            .findOne({ id: userId })
+        // .populate({
+        //     path:'Communities',
+        //     model:Community
+        // })
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user:${error?.message}`)
+    }
+}
+export async function fetchUserPosts(userId: string) {
+    try {
+        connectToDB();
+
+        const threads = await User.findOne({ id: userId }).populate({
+            path: "threads",
+            model: Thread,
+            populate: [
+                // {
+                //     path: "community",
+                //     model: Community,
+                //     select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
+                // },
+                {
+                    path: "children",
+                    model: Thread,
+                    populate: {
+                        path: "author",
+                        model: User,
+                        select: "name image id", // Select the "name" and "_id" fields from the "User" model
+                    },
+                },
+            ],
+        });
+        return threads;
+    } catch (error: any) {
         throw new Error(`Failed to fetch user:${error?.message}`)
     }
 }
